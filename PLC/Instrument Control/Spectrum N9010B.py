081697644743253dfabd800625650ea9fec041c8
Eddy <95586190+EddyAbzah@users.noreply.github.com>
2024-10-17 18:18:02 +0300

    Added new script to control Spectrum

---
+++ PLC/Instrument Control/Spectrum N9010B.py
@@ -0,0 +1,75 @@
+import pyvisa
+
+
+class KeysightN9010B:
+    def __init__(self, ip_address):
+        self.ip_address = ip_address
+        self.rm = pyvisa.ResourceManager()
+        self.instrument = None
+
+    def connect(self):
+        """Connect to the spectrum analyzer."""
+        resource_string = f'TCPIP0::{self.ip_address}::INSTR'
+        try:
+            self.instrument = self.rm.open_resource(resource_string)
+            idn = self.instrument.query('*IDN?')
+            print(f'Connected to: {idn}')
+        except pyvisa.VisaIOError as e:
+            print(f'Connection error: {e}')
+            raise
+
+    def disconnect(self):
+        """Disconnect from the spectrum analyzer."""
+        if self.instrument:
+            self.instrument.close()
+            print('Disconnected from the instrument.')
+
+    def reset(self):
+        """Reset the spectrum analyzer."""
+        self.instrument.write('*RST')
+
+    def print_all_commands(self):
+        """Prints all available SCPI commands."""
+        print(self.instrument.query(':SYST:HELP:HEAD?'))
+
+    def get_settings(self):
+        """Get the Impedance, Coupling, and AvgType."""
+        impedance = float(self.instrument.query(f'CORR:IMP?').strip())
+        print(f'{impedance = }')
+        coupling = self.instrument.query(f':INP:COUP?').strip()
+        print(f'{coupling = }')
+        avg_type = self.instrument.query(f':AVER:TYPE?').strip()
+        print(f'{avg_type = }')
+
+    def get_levels(self):
+        """Get the Attenuation, RefLevel, and YRefLevel."""
+        attenuation = float(self.instrument.query(f':SENS:POW:RF:ATT?').strip())
+        print(f'{attenuation = }')
+        ref_level = float(self.instrument.query('DISP:WIND:TRAC:Y:SCAL:RLEV:OFFS?').strip())
+        print(f'{ref_level = }')
+        y_ref_level = float(self.instrument.query('DISP:WIND:TRAC:Y:SCAL:RLEV?').strip())
+        print(f'{y_ref_level = }')
+
+    def get_frequencies(self):
+        """Get the StartFreq, StopFreq, and BW."""
+        freq_start = float(self.instrument.query(f'FREQ:START?').strip())
+        print(f'{freq_start = }')
+        freq_stop = float(self.instrument.query(f'FREQ:STOP?').strip())
+        print(f'{freq_stop = }')
+        freq_center = float(self.instrument.query(f'FREQ:CENTER?').strip())
+        print(f'{freq_center = }')
+        resolution_bandwidth = float(self.instrument.query(f':BAND:RES?').strip())
+        print(f'{resolution_bandwidth = }')
+        video_bandwidth = float(self.instrument.query(f':BAND:VID?').strip())
+        print(f'{video_bandwidth = }')
+
+
+if __name__ == '__main__':
+    analyzer = KeysightN9010B("10.20.30.32")
+    try:
+        analyzer.connect()
+        analyzer.get_settings()
+        analyzer.get_levels()
+        analyzer.get_frequencies()
+    finally:
+        analyzer.disconnect()
