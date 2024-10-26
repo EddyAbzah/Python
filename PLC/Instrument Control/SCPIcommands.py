e6c0d1d202b67424ff252fa6460a95280a9e5598
Eddy <95586190+EddyAbzah@users.noreply.github.com>
2024-10-26 18:57:52 +0300

    Added SCPI commands library

---
+++ PLC/Instrument Control/SCPIcommands.py
@@ -0,0 +1,13 @@
+scpi_commands = {
+    # Spectrum N9010B SCPI Commands:
+    "n9010b_impedance": "CORR:IMP",
+    "n9010b_coupling": ":INP:COUP",
+    "n9010b_avg_type": ":AVER:TYPE",
+    "n9010b_attenuation": ":SENS:POW:RF:ATT",
+    "n9010b_ref_level": "DISP:WIND:TRAC:Y:SCAL:RLEV:OFFS",
+    "n9010b_y_ref_level": "DISP:WIND:TRAC:Y:SCAL:RLEV",
+    "n9010b_freq_start": "FREQ:START",
+    "n9010b_freq_stop": "FREQ:STOP",
+    "n9010b_resolution_bandwidth": ":BAND:RES",
+    "n9010b_video_bandwidth": ":BAND:VID",
+}
--- PLC/Instrument Control/SpectrumN9010B.py
+++ PLC/Instrument Control/SpectrumN9010B.py
@@ -1,4 +1,5 @@
 import pyvisa
+from SCPIcommands import scpi_commands
 
 
 class KeysightN9010B:
@@ -50,7 +51,10 @@ class KeysightN9010B:
             print(f"All spectrum commands:\n{all_commands}")
         return all_commands
 
-    def check_mismatch(self, get_value, set_value):
+    def get_set_value(self, measurement, set_value):
+        if set_value is not None:
+            self.instrument.write(f'{scpi_commands["n9010b_" + measurement]} {set_value}')
+        get_value = self.instrument.query(f'{scpi_commands["n9010b_" + measurement]}?')
         if set_value is not None and get_value != set_value:
             if self.use_prints:
                 print(f'Mismatch in the get / set values!!! {get_value} != {set_value}')
@@ -61,88 +65,18 @@ class KeysightN9010B:
         else:
             return get_value
 
-    def impedance(self, set_value=None):
-        """Get or Set the Impedance [ohm]."""
-        if set_value is not None:
-            self.instrument.query(f'CORR:IMP?')
-        get_value = float(self.instrument.query(f'CORR:IMP?').strip())
-        return self.check_mismatch(set_value, get_value)
-
-    def coupling(self, set_value=None):
-        """Get or Set the Coupling [AC / DC]."""
-        if set_value is not None:
-            self.instrument.query(f':INP:COUP?')
-        get_value = self.instrument.query(f':INP:COUP?').strip()
-        return self.check_mismatch(set_value, get_value)
-
-    def avg_type(self, set_value=None):
-        """Get or Set the AvgType [LogPwr / RMS]."""
-        if set_value is not None:
-            self.instrument.query(f':AVER:TYPE?')
-        get_value = self.instrument.query(f':AVER:TYPE?').strip()
-        return self.check_mismatch(set_value, get_value)
-
-    def attenuation(self, set_value=None):
-        """Get or Set the Attenuation [dB]."""
-        if set_value is not None:
-            self.instrument.query(f':SENS:POW:RF:ATT?')
-        get_value = float(self.instrument.query(f':SENS:POW:RF:ATT?').strip())
-        return self.check_mismatch(set_value, get_value)
-
-    def ref_level(self, set_value=None):
-        """Get or Set the Reference Level [dBm]."""
-        if set_value is not None:
-            self.instrument.query(f'DISP:WIND:TRAC:Y:SCAL:RLEV:OFFS?')
-        get_value = float(self.instrument.query(f'DISP:WIND:TRAC:Y:SCAL:RLEV:OFFS?').strip())
-        return self.check_mismatch(set_value, get_value)
-
-    def y_ref_level(self, set_value=None):
-        """Get or Set the Y-axis Reference Level [dBm]."""
-        if set_value is not None:
-            self.instrument.query(f'DISP:WIND:TRAC:Y:SCAL:RLEV?')
-        get_value = float(self.instrument.query(f'DISP:WIND:TRAC:Y:SCAL:RLEV?').strip())
-        return self.check_mismatch(set_value, get_value)
-
-    def freq_start(self, set_value=None):
-        """Get or Set the Start Frequency [kHz]."""
-        if set_value is not None:
-            self.instrument.query(f'FREQ:START?')
-        get_value = float(self.instrument.query(f'FREQ:START?').strip())
-        return self.check_mismatch(set_value, get_value)
-
-    def freq_stop(self, set_value=None):
-        """Get or Set the Stop Frequency [kHz]."""
-        if set_value is not None:
-            self.instrument.query(f'FREQ:STOP?')
-        get_value = float(self.instrument.query(f'FREQ:STOP?').strip())
-        return self.check_mismatch(set_value, get_value)
-
-    def resolution_bandwidth(self, set_value=None):
-        """Get or Set the Resolution Bandwidth [kHz]."""
-        if set_value is not None:
-            self.instrument.query(f':BAND:RES?')
-        get_value = float(self.instrument.query(f':BAND:RES?').strip())
-        return self.check_mismatch(set_value, get_value)
-
-    def video_bandwidth(self, set_value=None):
-        """Get or Set the Video Bandwidth [kHz]."""
-        if set_value is not None:
-            self.instrument.query(f':BAND:VID?')
-        get_value = float(self.instrument.query(f':BAND:VID?').strip())
-        return self.check_mismatch(set_value, get_value)
-
 
 if __name__ == '__main__':
     spectrum = KeysightN9010B()
     if spectrum.connect("10.20.30.32"):
-        spectrum.impedance(set_value=None)
-        spectrum.coupling(set_value=None)
-        spectrum.avg_type(set_value=None)
-        spectrum.attenuation(set_value=None)
-        spectrum.ref_level(set_value=None)
-        spectrum.y_ref_level(set_value=None)
-        spectrum.freq_start(set_value=None)
-        spectrum.freq_stop(set_value=None)
-        spectrum.resolution_bandwidth(set_value=None)
-        spectrum.video_bandwidth(set_value=None)
+        spectrum.get_set_value("impedance", set_value=None)
+        spectrum.get_set_value("coupling", set_value=None)
+        spectrum.get_set_value("avg_type", set_value=None)
+        spectrum.get_set_value("attenuation", set_value=None)
+        spectrum.get_set_value("ref_level", set_value=None)
+        spectrum.get_set_value("y_ref_level", set_value=None)
+        spectrum.get_set_value("freq_start", set_value=None)
+        spectrum.get_set_value("freq_stop", set_value=None)
+        spectrum.get_set_value("resolution_bandwidth", set_value=None)
+        spectrum.get_set_value("video_bandwidth", set_value=None)
         spectrum.disconnect()
