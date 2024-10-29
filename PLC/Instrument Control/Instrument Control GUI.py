12b5917e635309a79c2f9dda04bdcc5e55fe84cb
Eddy <95586190+EddyAbzah@users.noreply.github.com>
2024-10-29 22:57:35 +0200

    Checked all commands
    Missing = Screen + Traces

--- PLC/Instrument Control/Instrument Control GUI.py
+++ PLC/Instrument Control/Instrument Control GUI.py
@@ -12,7 +12,6 @@ import SpectrumN9010B
 
 Window.size = (1200, 800)
 image_refresh_rate_ms = 1000
-test_types = ["Full range", "LF-PLC", "HF-PLC"]
 regex_ip_pattern = r'^(?:[0-9]{1,2}\.){3}[0-9]{1,3}$'      # pass from xx.xx.xx.xxx to x.x.x.x
 
 
@@ -21,27 +20,36 @@ class IPDialogContent(BoxLayout):
 
 
 class InstrumentControlGUI(MDApp):
-    # Initialize new spectrum instance from "SpectrumN9010B.py"
-    spectrum = SpectrumN9010B.KeysightN9010B()
-    # Disable terminal prints
-    spectrum.use_prints = False
-    # The slider range
-    spectrum_range_start = 0
-    spectrum_range_stop = 300
-    # Colors for the "connection status" in the top left
+    spectrum = SpectrumN9010B.KeysightN9010B()      # Initialize new spectrum instance from "SpectrumN9010B.py"
+    ip_address = "10.20.30.32"                      # default IP address for the Spectrum
+    spectrum.use_prints = False                     # Enable terminal prints
+    enable_hint_text = False                        # Enable grey text hints in the GUI's text inputs
+
+    # Colors for the "connection status" in the top left:
     color_red = [0.7, 0.1, 0.1, 0.7]
     color_green = [0.1, 0.5, 0.1, 0.7]
-    # NumericProperty binds any changes to the GUI
-    start_frequency = NumericProperty(1.0)
-    stop_frequency = NumericProperty(300.0)
+
+    # Scrollbars and their default values:
+    test_types = ["Default", "LF-PLC TX", "LF-PLC RX", "HF-PLC TX", "HF-PLC RX"]
+    test_type_default = test_types[2]
+    coupling_types = ["AC", "DC"]
+    coupling_type_default = coupling_types[1]
+    average_types = {"LogPwr": "LOG", "RMS": "RMS", "Voltage": "SCAL"}
+    average_type_default = list(average_types)[1]
+    trace_types = ["Average", "MaxHold", "AVG_MH"]
+    trace_type_default = trace_types[2]
+
+    # Default values:
+    spectrum_range_start = 0
+    spectrum_range_stop = 300
+    start_frequency = NumericProperty(1.0)      # NumericProperty binds any changes to the GUI
+    stop_frequency = NumericProperty(300.0)     # NumericProperty binds any changes to the GUI
     rbw = 1.0
     vbw = 1.0
     impedance = 50
     attenuation = 10
     reference_level = 10
     y_reference_level = 0
-    # default IP address for the Spectrum
-    ip_address = "10.20.30.32"
 
     def build(self):
         return Builder.load_file('Instrument Control GUI.kv')
@@ -53,9 +61,11 @@ class InstrumentControlGUI(MDApp):
         self.menu_test_type = MDDropdownMenu(
             caller=self.root.ids.test_type_dropdown,
             items=[
-                {"text": test_types[0], "viewclass": "OneLineListItem", "on_release": lambda x="Full range": self.set_test_type_item("Full range")},
-                {"text": test_types[1], "viewclass": "OneLineListItem", "on_release": lambda x="LF-PLC": self.set_test_type_item("LF-PLC")},
-                {"text": test_types[2], "viewclass": "OneLineListItem", "on_release": lambda x="HF-PLC": self.set_test_type_item("HF-PLC")}
+                {"text": self.test_types[0], "viewclass": "OneLineListItem", "on_release": lambda x=0: self.set_test_type_item(0)},
+                {"text": self.test_types[1], "viewclass": "OneLineListItem", "on_release": lambda x=1: self.set_test_type_item(1)},
+                {"text": self.test_types[2], "viewclass": "OneLineListItem", "on_release": lambda x=2: self.set_test_type_item(2)},
+                {"text": self.test_types[3], "viewclass": "OneLineListItem", "on_release": lambda x=3: self.set_test_type_item(3)},
+                {"text": self.test_types[4], "viewclass": "OneLineListItem", "on_release": lambda x=4: self.set_test_type_item(4)}
             ],
             width_mult=3,
         )
@@ -63,9 +73,11 @@ class InstrumentControlGUI(MDApp):
         # Initialize the Coupling dropdown
         self.menu_coupling = MDDropdownMenu(
             caller=self.root.ids.coupling_dropdown,
+            # Don't do this:
+            # items=[{"text": v, "viewclass": "OneLineListItem", "on_release": lambda x=i: self.set_coupling_item(v)} for i, v in enumerate(self.coupling_types)],
             items=[
-                {"text": "AC", "viewclass": "OneLineListItem", "on_release": lambda x="AC": self.set_coupling_item("AC")},
-                {"text": "DC", "viewclass": "OneLineListItem", "on_release": lambda x="DC": self.set_coupling_item("DC")}
+                {"text": self.coupling_types[0], "viewclass": "OneLineListItem", "on_release": lambda x=0: self.set_coupling_item(self.coupling_types[0])},
+                {"text": self.coupling_types[1], "viewclass": "OneLineListItem", "on_release": lambda x=1: self.set_coupling_item(self.coupling_types[1])}
             ],
             width_mult=3,
         )
@@ -74,9 +86,9 @@ class InstrumentControlGUI(MDApp):
         self.menu_avg_type = MDDropdownMenu(
             caller=self.root.ids.avg_type_dropdown,
             items=[
-                {"text": "Log", "viewclass": "OneLineListItem", "on_release": lambda x="Log": self.set_avg_type_item("Log")},
-                {"text": "Pwr", "viewclass": "OneLineListItem", "on_release": lambda x="Pwr": self.set_avg_type_item("Pwr")},
-                {"text": "Temp", "viewclass": "OneLineListItem", "on_release": lambda x="Temp": self.set_avg_type_item("Temp")}
+                {"text": list(self.average_types)[0], "viewclass": "OneLineListItem", "on_release": lambda x=0: self.set_avg_type_item(list(self.average_types.items())[0])},
+                {"text": list(self.average_types)[1], "viewclass": "OneLineListItem", "on_release": lambda x=1: self.set_avg_type_item(list(self.average_types.items())[1])},
+                {"text": list(self.average_types)[2], "viewclass": "OneLineListItem", "on_release": lambda x=2: self.set_avg_type_item(list(self.average_types.items())[2])}
             ],
             width_mult=3,
         )
@@ -85,9 +97,9 @@ class InstrumentControlGUI(MDApp):
         self.menu_traces = MDDropdownMenu(
             caller=self.root.ids.traces_dropdown,
             items=[
-                {"text": "Average", "viewclass": "OneLineListItem", "on_release": lambda x="Average": self.set_traces_item("Average")},
-                {"text": "MaxHold", "viewclass": "OneLineListItem", "on_release": lambda x="MaxHold": self.set_traces_item("MaxHold")},
-                {"text": "AVG_MH", "viewclass": "OneLineListItem", "on_release": lambda x="AVG_MH": self.set_traces_item("AVG_MH")}
+                {"text": self.trace_types[0], "viewclass": "OneLineListItem", "on_release": lambda x=0: self.set_traces_item(self.trace_types[0])},
+                {"text": self.trace_types[1], "viewclass": "OneLineListItem", "on_release": lambda x=1: self.set_traces_item(self.trace_types[1])},
+                {"text": self.trace_types[2], "viewclass": "OneLineListItem", "on_release": lambda x=2: self.set_traces_item(self.trace_types[2])}
             ],
             width_mult=3,
         )
@@ -142,18 +154,34 @@ class InstrumentControlGUI(MDApp):
         self.root.ids.connection_card.md_bg_color = self.color_green
         self.root.ids.connection_button.text = "Disconnect"
 
-    def set_test_type_item(self, item):
-        """Set the selected item in the Test type dropdown."""
-        self.root.ids.test_type_dropdown.text = item
-        if item == test_types[1]:
-            self.set_start_frequency(10)
-            self.set_stop_frequency(90)
-        elif item == test_types[2]:
-            self.set_start_frequency(20)
-            self.set_stop_frequency(80)
-        else:
-            self.set_start_frequency(0)
-            self.set_stop_frequency(100)
+    def set_test_type_item(self, item_number):
+        """Set the selected item in the Test type dropdown. Should be: "Default", "LF-PLC TX", "LF-PLC RX", "HF-PLC TX", and "HF-PLC RX"""
+
+        # TX:
+        # BW LF = 0.51
+        # BW HF = 5.1
+
+        # RX:
+        # BW LF = 0.68
+        # BW HF = 6.8
+
+        self.root.ids.test_type_dropdown.text = self.test_types[item_number]
+        match item_number:
+            case 0:         # Default
+                self.set_start_frequency(1)
+                self.set_stop_frequency(10e3)
+            case 1:         # LF-PLC TX
+                self.set_start_frequency(50)
+                self.set_stop_frequency(70)
+            case 2:         # LF-PLC RX
+                self.set_start_frequency(1)
+                self.set_stop_frequency(300)
+            case 3:         # HF-PLC TX
+                self.set_start_frequency(1)
+                self.set_stop_frequency(150)
+            case _:         # HF-PLC RX
+                self.set_start_frequency(10)
+                self.set_stop_frequency(5e3)
         self.menu_test_type.dismiss()
 
     def set_coupling_item(self, item):
@@ -163,7 +191,8 @@ class InstrumentControlGUI(MDApp):
 
     def set_avg_type_item(self, item):
         """Set the selected item in the Average type dropdown."""
-        self.root.ids.avg_type_dropdown.text = item
+        item_key, item_value = item
+        self.root.ids.avg_type_dropdown.text = item_key
         self.menu_avg_type.dismiss()
 
     def set_traces_item(self, item):
--- PLC/Instrument Control/SpectrumN9010B.py
+++ PLC/Instrument Control/SpectrumN9010B.py
@@ -1,3 +1,18 @@
+"""
+Script to control the Keysight N9010B. Commands:
+Impedance: choose between 50ohm or 75ohm.
+Coupling: choose AC or DC.
+Average Type: 3 options: LogPwr (LOG), RMS, Voltage (SCAL).
+Attenuation: enter number in dB; default = 10dB.
+Reference Level: enter number in dBm; default = 0dBm. Set to 10 to offset for external 100ohm, or to 20 to offset for external 500ohm.
+Y-axis Reference Level: enter number in dBm; default = 0dBm. This only affects the visuals, not the measurements.
+Start Frequency: enter number in kHz.
+Stop Frequency: enter number in kHz.
+Resolution Bandwidth: enter number in kHz.
+Video Bandwidth: enter number in kHz.
+"""
+
+
 # install PyVISA-py or use Keysight IO Library
 import pyvisa
 from SCPIcommands import scpi_commands, scpi_syntax
@@ -53,13 +68,14 @@ class KeysightN9010B:
         return all_commands
 
     def get_set_value(self, measurement, set_value):
+        """Get the SCPI command from the SCPIcommands.py file, and send to the Spectrum."""
         if set_value is not None:
             self.instrument.write(f'{scpi_commands["n9010b_" + measurement]} {set_value}')
         get_value = self.instrument.query(f'{scpi_commands["n9010b_" + measurement]}?').strip()
         try:
             get_value = float(get_value)
         except ValueError:
-            pass
+            pass        # this is OK... get_value is string
         if set_value is not None and get_value != set_value:
             if self.use_prints:
                 print(f'Mismatch in the get / set values!!! {get_value} != {set_value}')
@@ -70,18 +86,43 @@ class KeysightN9010B:
         else:
             return get_value
 
+    def traces_set(self, measurement, set_value):
+        """Set the trace type(s): Average, MaxHold, or both."""
+        pass
+
+    def traces_run(self, measurement, set_value):
+        """Run (refresh) the traces."""
+        pass
+
+    def traces_stop(self, measurement, set_value):
+        """Stop the traces."""
+        pass
+
 
 if __name__ == '__main__':
+    get_only = True
     spectrum = KeysightN9010B()
     if not spectrum.connect("10.20.30.32"):
-        spectrum.get_set_value("impedance", set_value=None)
-        spectrum.get_set_value("coupling", set_value=None)
-        spectrum.get_set_value("avg_type", set_value=None)
-        spectrum.get_set_value("attenuation", set_value=None)
-        spectrum.get_set_value("ref_level", set_value=None)
-        spectrum.get_set_value("y_ref_level", set_value=None)
-        spectrum.get_set_value("freq_start", set_value=None)
-        spectrum.get_set_value("freq_stop", set_value=None)
-        spectrum.get_set_value("resolution_bandwidth", set_value=None)
-        spectrum.get_set_value("video_bandwidth", set_value=None)
+        if get_only:
+            spectrum.get_set_value("impedance", set_value=None)
+            spectrum.get_set_value("coupling", set_value=None)
+            spectrum.get_set_value("avg_type", set_value=None)
+            spectrum.get_set_value("attenuation", set_value=None)
+            spectrum.get_set_value("ref_level", set_value=None)
+            spectrum.get_set_value("y_ref_level", set_value=None)
+            spectrum.get_set_value("freq_start", set_value=None)
+            spectrum.get_set_value("freq_stop", set_value=None)
+            spectrum.get_set_value("resolution_bandwidth", set_value=None)
+            spectrum.get_set_value("video_bandwidth", set_value=None)
+        else:
+            spectrum.get_set_value("impedance", set_value=50)
+            spectrum.get_set_value("coupling", set_value="DC")
+            spectrum.get_set_value("avg_type", set_value="RMS")
+            spectrum.get_set_value("attenuation", set_value=10)
+            spectrum.get_set_value("ref_level", set_value=0)
+            spectrum.get_set_value("y_ref_level", set_value=0)
+            spectrum.get_set_value("freq_start", set_value=10e3)
+            spectrum.get_set_value("freq_stop", set_value=300e3)
+            spectrum.get_set_value("resolution_bandwidth", set_value=500)
+            spectrum.get_set_value("video_bandwidth", set_value=5000)
         spectrum.disconnect()
