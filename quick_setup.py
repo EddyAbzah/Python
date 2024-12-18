32171d9bfbcfaed38bd3d014ac687c1061d4917e
Eddy <95586190+EddyAbzah@users.noreply.github.com>
2024-12-18 15:50:47 +0200

    Update quick_setup.py

--- quick_setup.py
+++ quick_setup.py
@@ -7,7 +7,12 @@ import datetime, requests, warnings, threading, functools, matplotlib, statistic
 import pandas as pd, plotly as py, openpyxl as px, numpy as np, plotly.express as px, scipy.signal as signal, plotly.graph_objs as go
 import matplotlib.pyplot as plt, plotly.graph_objects as go, plotly.figure_factory as ff
 
-file_path = r""
-df = pd.read_csv(file_path)
-print(f'{df.head(5) = }')
+file_path1 = r""
+file_path2 = r""
+if file_path1 != "":
+    df1 = pd.read_csv(file_path1)
+    print(f'df1.columns = {list(df1.columns)}')
+if file_path2 != "":
+    df2 = pd.read_csv(file_path2)
+    print(f'df2.columns = {list(df2.columns)}')
 exit()
