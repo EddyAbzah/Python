02a365dabebea7c3ed62b5e70063fb3ebab047ff
Eddy <95586190+EddyAbzah@users.noreply.github.com>
2024-12-18 15:43:06 +0200

    Create quick_setup.py

---
+++ quick_setup.py
@@ -0,0 +1,13 @@
+import my_tools
+import Plot_Graphs_with_Sliders as _G
+from my_pyplot import omit_plot as _O, plot as _P, print_chrome as _PC, clear as _PP, print_lines as _PL, send_mail as _SM
+
+import io, gc, os, re, sys, glob, math, time, cmath, pylab, queue, scipy, shutil, struct, asyncio, filecmp, inspect, pathlib, smtplib
+import datetime, requests, warnings, threading, functools, matplotlib, statistics, webbrowser, progressbar, multipledispatch
+import pandas as pd, plotly as py, openpyxl as px, numpy as np, plotly.express as px, scipy.signal as signal, plotly.graph_objs as go
+import matplotlib.pyplot as plt, plotly.graph_objects as go, plotly.figure_factory as ff
+
+file_path = r""
+df = pd.read_csv(file_path)
+print(f'{df.head(5) = }')
+exit()
