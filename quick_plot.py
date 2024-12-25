b447af64f2dccee53634f35f4acff26dce9d05ea
Eddy <95586190+EddyAbzah@users.noreply.github.com>
2024-12-25 13:10:31 +0200

    Update quick_plot.py

--- quick_plot.py
+++ quick_plot.py
@@ -7,14 +7,17 @@ import datetime, requests, warnings, threading, functools, matplotlib, statistic
 import pandas as pd, plotly as py, openpyxl as px, numpy as np, plotly.express as px, scipy.signal as signal, plotly.graph_objs as go
 import matplotlib.pyplot as plt, plotly.graph_objects as go, plotly.figure_factory as ff
 
-file_paths = [r""]
+
+folder_paths = [r""]
+pattern = re.compile(r"")
 output_html__auto_open = [True, True]
+file_paths = [os.path.join(folder_path, f) for folder_path in folder_paths for f in os.listdir(folder_path) if pattern.match(f)]
+
+
 for file_index, file_path in enumerate(file_paths):
-    if file_path != "":
-        print(f"file_index = {file_index + 1}: {file_path}")
-        df = pd.read_csv(file_path)
-        print(f"df.columns = {list(df.columns)}")
-        if output_html__auto_open[0]:
-            _PC(df, path=str(pathlib.Path(file_path).parent), file_name=pathlib.Path(file_path).stem, title=pathlib.Path(file_path).stem, auto_open=output_html__auto_open[1])
-            print(f"Output HTML to {str(pathlib.Path(file_path).with_suffix('.html'))}")
-exit()
+    print(f"file_index = {file_index + 1}: {file_path}")
+    df = pd.read_csv(file_path)
+    print(f"df.columns = {list(df.columns)}")
+    if output_html__auto_open[0]:
+        _PC(df, path=str(pathlib.Path(file_path).parent), file_name=pathlib.Path(file_path).stem, title=pathlib.Path(file_path).stem, auto_open=output_html__auto_open[1])
+        print(f"Output HTML to {str(pathlib.Path(file_path).with_suffix('.html'))}")
