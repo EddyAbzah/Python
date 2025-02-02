235d75592ea11293dd95008183ee6f150d521763
Eddy <95586190+EddyAbzah@users.noreply.github.com>
2025-02-03 00:31:32 +0200

    Update Scrap IMDb.py

--- DSP/Other/Scrap IMDb.py
+++ DSP/Other/Scrap IMDb.py
@@ -27,9 +27,12 @@ def get_episode_titles():
     return episode_titles
 
 
-def rename_episodes(episode_titles):
+def rename_episodes(episode_titles, just_print=False):
     """Rename episode files based on IMDb episode titles."""
-    files = sorted([f for f in os.listdir(directory) if f.lower().endswith(('mp4', 'mkv', 'avi'))])
+    files = [f for f in os.listdir(directory) if f.lower().endswith(('mp4', 'mkv', 'avi'))]
+    if sort_episode_titles:
+        files.sort()
+
 
     if len(files) != len(episode_titles):
         print("Warning: Number of files doesn't match the number of episodes!")
@@ -42,21 +45,24 @@ def rename_episodes(episode_titles):
 
         file_ext = os.path.splitext(filename)[1]
         new_name = f"S{season:02}E{episode_number:02} - {episode_titles[episode_number]}{file_ext}"
-        old_path = os.path.join(directory, filename)
-        new_path = os.path.join(directory, new_name)
-
-        os.rename(old_path, new_path)
-        print(f"Renamed: {filename} → {new_name}")
+        if just_print:
+            print(f"{filename} → {new_name}")
+        else:
+            old_path = os.path.join(directory, filename)
+            new_path = os.path.join(directory, new_name)
+            os.rename(old_path, new_path)
+            print(f"Renamed: {filename} → {new_name}")
 
 
 if __name__ == '__main__':
     directory = r""
     imdb_id = ""
     season = 1
+    sort_episode_titles = False     # sorting takes case sensitivity into account
 
     titles = get_episode_titles()
-    print(titles)
-
+    if titles:
+        rename_episodes(titles, just_print=True)
     user_input = input("Do you want to continue? (yes / no): ")
-    if "y" in user_input.lower() and titles:
+    if "y" in user_input.lower():
         rename_episodes(titles)
