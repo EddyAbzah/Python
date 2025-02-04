eee96fb72a95994b4f323097b9d39a66e9379088
Eddy <95586190+EddyAbzah@users.noreply.github.com>
2025-02-05 01:31:46 +0200

    MPV Player playlist now prefect

--- DSP/Other/MPV Player/Toggle videos.lua
+++ DSP/Other/MPV Player/Toggle videos.lua
@@ -1,7 +1,15 @@
+-- IMPORTANT
+-- To update the bind keys to the current playlist, press "Shift+F1"
+
+
+
 -- Initial settings:
 mp.set_property("osd-font-size", 20)
-local delay = 0.05
-local last_position = 0
+local delay = 0.05						-- delay to set the time position correctly 
+local last_position = 0					-- to save the time position of the current file
+local set_intial_playlist = false		-- to set the playlist at stratup
+local initial_bind_count = 8			-- bind this many keys (so you wouldn't have to reset the bind keys)
+
 
 
 -- Initial files to load:
@@ -15,29 +23,33 @@ local file_list = {
 
 -- Load files:
 local function load_file(index)
-    local file = file_list[index]
-    if file then
-        mp.osd_message(file, 2)
-        last_position = mp.get_property_number("time-pos") or 0
-        --mp.commandv("loadfile", file, "replace")
-        mp.commandv("playlist-play-index", index - 1)
-        mp.add_timeout(delay, function()
-            mp.set_property_number("time-pos", last_position)
-        end)
-    else
-        mp.osd_message("Invalid index: " .. index, 2)
-    end
+	if index <= #file_list then				-- Lua uses 1-based indexing
+		local file = file_list[index]
+		if #file > 0 then					-- # = length
+			last_position = mp.get_property_number("time-pos") or 0
+			mp.commandv("playlist-play-index", index - 1)
+			mp.osd_message(file, 2)
+			mp.add_timeout(delay, function()
+				mp.set_property_number("time-pos", last_position)
+			end)
+		else
+			mp.osd_message("No file in playlist for key: F" .. index .. "\nPress Shift+F1 to update the playlist", 4)
+		end
+	else
+		mp.osd_message("No file in playlist for key: F" .. index .. "\nPress Shift+F1 to update the playlist", 4)
+	end
 end
 
 
 -- Bind keys for loading files:
-local function bind_keys()
-    for i = 1, #file_list do
+local function bind_keys(bind_keys_count)
+    for i = 1, bind_keys_count do
         local key = "F" .. i
         mp.add_key_binding(key, "load_file_" .. key, function()
             load_file(i)
         end)
     end
+	mp.osd_message("Playlist updated. Items: " .. #file_list, 2)
 end
 
 
@@ -48,19 +60,26 @@ local function update_file_list()
     for _, item in ipairs(playlist) do
         table.insert(file_list, item.filename)
     end
-	bind_keys()
-    mp.osd_message("Playlist updated. Items: " .. #file_list, 2)
+	bind_keys(#file_list)
 end
 
 
 -- Set the initial playlist without playing
-for i, file in ipairs(file_list) do
-	if file and string.len(file) > 0 then
-		mp.commandv("loadfile", file, "append")
+local function initial_playlist()
+	for i, file in ipairs(file_list) do
+		if file and string.len(file) > 0 then
+			mp.commandv("loadfile", file, "append")
+		end
 	end
+	bind_keys(#file_list)
 end
 
 
 -- Initial setup:
-bind_keys()
+if set_intial_playlist then
+	initial_playlist()
+else
+    bind_keys(initial_bind_count)
+end
 mp.add_key_binding("Shift+F1", "update_file_list", update_file_list)
+update_file_list()
