bdfc573afa9e37f2b1a8064b812ce232de769b2f
Eddy <95586190+EddyAbzah@users.noreply.github.com>
2025-06-27 14:56:24 +0300

    DST is now automatic

--- DSP/Other/Pixel Rename Files.py
+++ DSP/Other/Pixel Rename Files.py
@@ -1,17 +1,19 @@
 import os
 import re
-from datetime import datetime, timedelta
+from zoneinfo import ZoneInfo
+from datetime import datetime, timezone
 
 
 directory = r""
 pattern_in = r"PXL_(\d{8})_(\d{9})"
 pattern_out = "%Y-%m-%d _ %H-%M-%S"
+time_zone = "Asia/Jerusalem"
+# time_zone = "Asia/Tbilisi"
+
 new_name_prefix = ""
 new_name_suffix = ""
-
-check_if_dst = [True, datetime(2025, 3, 28), datetime(2025, 10, 26)]
-time_delta = timedelta(days=0, hours=2, seconds=0, minutes=0)
-time_delta_dst = timedelta(days=0, hours=3, seconds=0, minutes=0)       # Daylight Saving(s) Time
+check_if_edit = [True, "~", " (edit)"]                  # enable, what to look for, suffix if found
+check_if_moving_picture = [True, ".MP", " (MP)"]        # enable, what to look for, suffix if found
 
 index_start = 1
 index_count = 666666666
@@ -29,22 +31,24 @@ if __name__ == '__main__':
         match = re.match(pattern_in, filename)
         if match:
             _, file_extension = os.path.splitext(filename)
-            original_date = datetime.strptime(match.group(1), "%Y%m%d")
-            original_time = datetime.strptime(match.group(2), "%H%M%S%f").time()
-            original_datetime = datetime.combine(original_date, original_time)
+            original_utc_date = datetime.strptime(match.group(1), "%Y%m%d")
+            original_utc_time = datetime.strptime(match.group(2), "%H%M%S%f").time()
+            original_utc_datetime = datetime.combine(original_utc_date, original_utc_time).replace(tzinfo=timezone.utc)
 
-            if check_if_dst[0]:
-                new_datetime = original_datetime + (time_delta_dst if check_if_dst[1] <= original_date <= check_if_dst[2] else time_delta)
-            else:
-                new_datetime = original_datetime + time_delta
+            new_datetime = original_utc_datetime.astimezone(ZoneInfo(time_zone))
             new_filename = new_name_prefix
             new_filename += new_datetime.strftime(pattern_out)
             new_filename += new_name_suffix
+
+            if check_if_edit[0] and check_if_edit[1] in filename:
+                new_filename += check_if_edit[2]
+            if check_if_moving_picture[0] and check_if_moving_picture[1] in filename:
+                new_filename += check_if_moving_picture[2]
             if file_extension.lower() == ".jpeg":
                 file_extension = ".jpg"
             new_filename += file_extension.lower()
 
-            file_info = {"original_name": filename, "new_name": new_filename, "date": original_datetime}
+            file_info = {"original_name": filename, "new_name": new_filename, "date": original_utc_datetime}
             file_data.append(file_info)
 
     existing_files = os.listdir(directory)
