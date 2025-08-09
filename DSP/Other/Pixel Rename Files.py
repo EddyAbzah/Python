9ad1c956fbf6bcc42922fcf89064109b0e63fb92
Eddy <95586190+EddyAbzah@users.noreply.github.com>
2025-08-09 20:04:54 +0300

    Getting files from Android

--- DSP/Other/Pixel Rename Files.py
+++ DSP/Other/Pixel Rename Files.py
@@ -11,7 +11,7 @@ time_zone = "Asia/Jerusalem"
 
 new_name_prefix = ""
 new_name_suffix = ""
-check_if_edit = [True, "~", " (edit)"]                  # enable, what to look for, suffix if found
+check_if_edit = [False, "~", " (edit)"]                  # enable, what to look for, suffix if found
 check_if_moving_picture = [False, ".MP", " (MP)"]       # enable, what to look for, suffix if found
 rename_in_reverse_order = True
 
@@ -21,6 +21,38 @@ rename = False
 # rename = True
 
 
+def get_renaming_scheme(filename):
+    match = re.match(pattern_in, filename)
+    if match:
+        _, file_extension = os.path.splitext(filename)
+        original_utc_date = datetime.strptime(match.group(1), "%Y%m%d")
+        original_utc_time = datetime.strptime(match.group(2), "%H%M%S%f").time()
+        original_utc_datetime = datetime.combine(original_utc_date, original_utc_time).replace(tzinfo=timezone.utc)
+
+        new_datetime = original_utc_datetime.astimezone(ZoneInfo(time_zone))
+        new_filename = new_name_prefix
+        new_filename += new_datetime.strftime(pattern_out)
+        new_filename += new_name_suffix
+
+        if check_if_edit[0] and check_if_edit[1] in filename:
+            new_filename += check_if_edit[2]
+        if check_if_moving_picture[0] and check_if_moving_picture[1] in filename:
+            new_filename += check_if_moving_picture[2]
+        if file_extension.lower() == ".jpeg":
+            file_extension = ".jpg"
+        new_filename += file_extension.lower()
+
+        return {"original_name": filename, "new_name": new_filename, "date": original_utc_datetime}
+
+
+def check_for_duplicates(file_data, destination_directory):
+    existing_files = os.listdir(destination_directory)
+    duplicates = [file["new_name"] for file in file_data if file["new_name"] in existing_files]
+    if duplicates:
+        print(f"Duplicate files found: {', '.join(duplicates)}")
+    return bool(duplicates)
+
+
 if __name__ == '__main__':
     files = os.listdir(directory)
     files.sort()
@@ -28,33 +60,10 @@ if __name__ == '__main__':
     for index, filename in enumerate(files):
         if index + 1 < index_start or index - index_start + 2 > index_count:
             continue
-        match = re.match(pattern_in, filename)
-        if match:
-            _, file_extension = os.path.splitext(filename)
-            original_utc_date = datetime.strptime(match.group(1), "%Y%m%d")
-            original_utc_time = datetime.strptime(match.group(2), "%H%M%S%f").time()
-            original_utc_datetime = datetime.combine(original_utc_date, original_utc_time).replace(tzinfo=timezone.utc)
+        file_info = get_renaming_scheme(filename)
+        file_data.append(file_info)
 
-            new_datetime = original_utc_datetime.astimezone(ZoneInfo(time_zone))
-            new_filename = new_name_prefix
-            new_filename += new_datetime.strftime(pattern_out)
-            new_filename += new_name_suffix
-
-            if check_if_edit[0] and check_if_edit[1] in filename:
-                new_filename += check_if_edit[2]
-            if check_if_moving_picture[0] and check_if_moving_picture[1] in filename:
-                new_filename += check_if_moving_picture[2]
-            if file_extension.lower() == ".jpeg":
-                file_extension = ".jpg"
-            new_filename += file_extension.lower()
-
-            file_info = {"original_name": filename, "new_name": new_filename, "date": original_utc_datetime}
-            file_data.append(file_info)
-
-    existing_files = os.listdir(directory)
-    duplicates = [file["new_name"] for file in file_data if file["new_name"] in existing_files]
-    if duplicates:
-        print(f"Duplicate files found: {', '.join(duplicates)}")
+    if check_for_duplicates(file_data, directory):
         input("Please resolve the duplicates before proceeding. Press Enter to continue.")
 
     file_data.sort(key=lambda x: x["date"], reverse=rename_in_reverse_order)
