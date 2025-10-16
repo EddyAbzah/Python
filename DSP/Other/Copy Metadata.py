07a8a17491dd2332294d097d1f557e225e38b489
Eddy <95586190+EddyAbzah@users.noreply.github.com>
2025-10-16 15:16:58 +0300

    Added subfolders

--- DSP/Other/Copy Metadata.py
+++ DSP/Other/Copy Metadata.py
@@ -1,4 +1,5 @@
 import os
+import re
 from pymediainfo import MediaInfo
 from mutagen.mp4 import MP4
 import shutil
@@ -15,12 +16,28 @@ def get_all_metadata(file_path):
     return metadata
 
 
+def convert_coordinates(coordinate_str):
+    pattern = r'([+-]?\d{1,2}\.\d+)([+-]?\d{1,3}\.\d+)'
+    if match := re.match(pattern, coordinate_str):
+        lat, lon = match.groups()
+        lat = float(lat)
+        lon = float(lon)
+        lat_direction = 'N' if lat > 0 else 'S'
+        lon_direction = 'E' if lon > 0 else 'W'
+        lat = abs(lat)
+        lon = abs(lon)
+        return f"{lat:.2f}°{lat_direction} {lon:.2f}°{lon_direction}"
+    else:
+        return coordinate_str
+
+
 def set_video_metadata(target_file, metadata):
     target_video = MP4(target_file)
     for key, value in metadata.items():
         if "xyz" in key:
             print(f'{key = }, {value = }')
             target_video[key] = value
+            target_video["@LOC"] = convert_coordinates(value)
 
         elif "encoded_date" in key:
             print(f'{key = }, {value = }')
@@ -28,19 +45,21 @@ def set_video_metadata(target_file, metadata):
     target_video.save()
 
 
-directory_source = r""
-directory_target = r""
+directory_source = r"C:\Users\eddya\Downloads\Pixel Media"
+directory_target = r"C:\Users\eddya\Videos"
 true_if_video = True
 copy_file_first = False
-files = [f for f in os.listdir(directory_source) if f.lower().endswith(".mp4" if true_if_video else ".jpg")]
 file_filter = ""
 
+files = [os.path.join(root, f) for root, _, filenames in os.walk(directory_source) for f in filenames if f.lower().endswith(".mp4" if true_if_video else ".jpg")]
+# files = [os.path.join(directory_source, f) for f in os.listdir(directory_source) if f.lower().endswith(".mp4" if true_if_video else ".jpg")]
 
-for file in files:
-    if file_filter not in file:
+
+for file_original in files:
+    filename = os.path.basename(file_original)
+    if file_filter not in filename:
         continue
-    file_original = os.path.join(directory_source, file)
-    file_new = os.path.join(directory_target, file)
+    file_new = os.path.join(directory_target, filename)
     if copy_file_first:
         shutil.copy(file_original, file_new)
     try:
